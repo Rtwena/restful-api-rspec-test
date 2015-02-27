d68427466052bf775e68f6dd73aa3ea3ff0710fd
Richard Twena <Richardtwena@gmail.com>
2015-02-27 12:19:09 +0000

    created test for data input from yaml file

--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -15,20 +15,35 @@ describe "ToDo" do
   describe "create/delete item" do
     it "should POST data into collection" do
       rpost = HTTParty.post url, query:{title: 'newthing!', due: '2015-01-25'}
-      rdel = HTTParty.delete url + "#{rpost["id"]}"
-
+      #Expectations
       expect(rpost.code).to eq(201)
       expect(rpost.message).to eq("Created")
+      #Teardown
+      rdel = HTTParty.delete url + "#{rpost["id"]}"
     end
     it "should DELETE item" do
       rpost = HTTParty.post url, query:{title: 'newthing!', due: '2015-01-25'}
       rdelete = HTTParty.delete url + "#{rpost["id"]}"
-
+      #Expectations
       expect(rdelete.code).to eq(204)
       expect(rdelete.message).to eq("No Content")
     end
+    it "should allow multiple data input from a yaml file" do
+      data = YAML.load(File.open('data/todo.yml'))
+      responses = []
+      data["items"].each do |item|
+        responses << (HTTParty.post url, query:{title: item["title"], due: item["due"]})
+      end
+
+      responses.each do |response|
+        #Expectations
+        expect(response.code).to eq(201)
+        expect(response.message).to eq("Created")
+        #Teardown
+        HTTParty.delete url + "#{response["id"]}"
+      end
+    end
   end
-  describe 
 end 
 
 
