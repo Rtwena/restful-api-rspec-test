898d78891e2a32651dbde23f86668864073fd75f
Richard Twena <Richardtwena@gmail.com>
2015-02-27 10:44:26 +0000

    added test create/delete

--- spec/todo_spec.rb
+++ spec/todo_spec.rb
@@ -3,12 +3,33 @@ require 'httparty'
 require 'yaml'
 
 describe "ToDo" do
+  let(:url) {url = "http://lacedeamon.spartaglobal.com/todos/"}
+  describe "get response from todos collection" do
+    it "should GET 200 'OK'" do
+      rget = HTTParty.get url
 
-  let(:url) {url = "http://lacedeamon.spartaglobal.com/todos"}
-  it "Should get 200 'OK'" do
-    rget = HTTParty.get url + "/300"
-
-    expect(rget.code).to eq(200)
-    expect(rget.message).to eq("OK")
+      expect(rget.code).to eq(200)
+      expect(rget.message).to eq("OK")
+    end
   end
-end
+  describe "create/delete item" do
+    it "should POST data into collection" do
+      rpost = HTTParty.post url, query:{title: 'newthing!', due: '2015-01-25'}
+      rdel = HTTParty.delete url + "#{rpost["id"]}"
+
+      expect(rpost.code).to eq(201)
+      expect(rpost.message).to eq("Created")
+    end
+    it "should DELETE item" do
+      rpost = HTTParty.post url, query:{title: 'newthing!', due: '2015-01-25'}
+      rdelete = HTTParty.delete url + "#{rpost["id"]}"
+
+      expect(rdelete.code).to eq(204)
+      expect(rdelete.message).to eq("No Content")
+    end
+  end
+  describe 
+end 
+
+
+#r = HTTParty.get "http://lacedeamon.spartaglobal.com/todos#{response[0]["id"]}"
